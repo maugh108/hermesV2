09f606b2c473c476dc30d826029a07b15792738f
maugh108 <maugh108@outlook.com>
2023-01-09 21:50:54 -0600

    dashboard modificada

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,43 +3,40 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 
 router.get('/', (req, res) => {
-    Driver.findAll({
-        attributes: [
-          'id',
-          'name',
-          'birthday',
-          'license',
-          'city',
-          'expiration',
-          'usermame',
-          'password',
-          'phone'
-        ],
-        include: [
-          {
-            model: Order,
-            attributes: ['id', 'driver', 'pickup', 'delivery', 'truck', '',],
-            include: {
-              model: Driver,
-              attributes: ['name']
-            }
-          },
-          {
-            model: Driver,
-            attributes: ['name']
-          }
-        ]
-      })
+    /*Driver.findAll({})
         .then(dbPostData => {
-        
-          const posts = dbPostData.map(post => post.get({ plain: true }));
-          res.render('dashboard', { posts, loggedIn: req.session.loggedIn });
+          //console.log(res.json(dbPostData));
+          const drivers = dbPostData.map(driver => driver.get({ plain: true }));
+          res.render('dashboard', { drivers, loggedIn: req.session.loggedIn });
         })
         .catch(err => {
           console.log(err);
           res.status(500).json(err);
         });
+    });*/
+
+    Order.findAll({
+      attributes: [
+        'id'
+      ],
+      
+      include: [
+        {
+          model: Driver,
+          attributes: ['id', 'name', 'birthday', 'license', 'city', 'phone']
+        }
+      ]
+    })
+    .then(dbPostData => {
+      
+      const orders = dbPostData.map(order => order.get({ plain: true }));
+      res.render('dashboard', { orders, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
+});
 
 router.get('/login', (req, res) => {
     if(req.session.loggedIn) {
